9d63bfe71a1ee940655c14738703fe754fd45201
jensw2015 <kawi@kawi-PC>
2016-03-14 13:53:35 +0100

    enum

--- medienverwaltung2/src/data/speicherorte/enums/BuchArt.java
+++ medienverwaltung2/src/data/speicherorte/enums/BuchArt.java
@@ -1,10 +1,39 @@
 package data.speicherorte.enums;
 
-public enum BuchArt {
-	HardCover,
-	SoftCover,
-	Taschenbuch,
-	Magazin,
-	Manga,
-	Comic;
+import interfaces.SelectAble;
+
+public enum BuchArt implements SelectAble {
+	HardCover	("Hardcover",   1),
+	SoftCover 	("Softcover",   2),
+	Taschenbuch	("Taschenbuch",	3),
+	Magazin 	("Magazin",		4),
+	Manga  		("Manga",		5),
+	Comic 		("Comic", 		6);
+	
+	private int id;
+	private String bezeichnung;
+	
+	private BuchArt(String bezeichnung, int id){
+		this.bezeichnung = bezeichnung;
+		this.id = id;
+	}
+
+	@Override
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	@Override
+	public int getId() {
+		return id;
+	}
+	
+	public static BuchArt getElementFromId(int id) {
+		for (BuchArt element : values()) {
+			if (element.getId() == id) {
+				return element;
+			}
+		}
+		return null;
+	}
 }
