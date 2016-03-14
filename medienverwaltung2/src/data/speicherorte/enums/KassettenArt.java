e1fc01eaecd96f74dac36597163998582c2341c7
jensw2015 <kawi@kawi-PC>
2016-03-14 20:31:28 +0100

    enums

--- medienverwaltung2/src/data/speicherorte/enums/KassettenArt.java
+++ medienverwaltung2/src/data/speicherorte/enums/KassettenArt.java
@@ -1,6 +1,39 @@
 package data.speicherorte.enums;
 
-public enum KassettenArt {
-	MusikKassette,
-	VideoKassette;
+import interfaces.SelectAble;
+
+
+public enum KassettenArt implements SelectAble {
+	MusikKassette	("Musikkassette", 1),
+	VideoKassette	("Videokassette", 2);
+		
+	private String 	bezeichnung;
+	private int 	id;
+	
+	private KassettenArt(String bezeichnung, int id) {
+		this.id 		 = id;
+		this.bezeichnung = bezeichnung;
+	}
+	
+	
+	@Override
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	@Override
+	public int getId() {
+		return id;
+	}
+	
+	public static KassettenArt getElementFromId(int id){
+		for (KassettenArt element : values()){
+			if (element.getId() == id){
+				return element;
+			}
+		}
+		return null;
+	}
+	
+	
 }
--- medienverwaltung2/src/data/speicherorte/enums/SchallplatteArt.java
+++ medienverwaltung2/src/data/speicherorte/enums/SchallplatteArt.java
@@ -1,7 +1,38 @@
 package data.speicherorte.enums;
 
-public enum SchallplatteArt {
-	Single,
-	MaxiSingle,
-	ExtendedPlay;
+import interfaces.SelectAble;
+
+public enum SchallplatteArt implements SelectAble{
+	Single			("Single",	1),
+	//Maxi Single = EP (Extended Play)
+	MaxiSingle		("EP",		2),
+	// ExtendedPlay = LongPlay(Langspielplatte)
+	ExtendedPlay	("LP",		3);
+	
+	private String 	bezeichnung;
+	private int 	id;
+	
+	private SchallplatteArt (String bezeichnung, int id){
+		this.bezeichnung = bezeichnung;
+		this.id			 = id;
+	}
+
+	@Override
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	@Override
+	public int getId() {
+		return id;
+	}
+	
+	public static SchallplatteArt getElementFromId (int id){
+		for (SchallplatteArt element : values()){
+			if(element.getId() == id){
+				return element;
+			}
+		}
+		return null;
+	}
 }
