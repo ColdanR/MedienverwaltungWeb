996c54fb634fb3bdd04446d49bbb8d888662ec49
jensw2015 <kawi@kawi-PC>
2016-03-15 12:11:49 +0100

    y

--- medienverwaltung2/src/data/medien/enums/HoerbuchArt.java
+++ medienverwaltung2/src/data/medien/enums/HoerbuchArt.java
@@ -3,7 +3,14 @@ package data.medien.enums;
 import interfaces.SelectAble;
 
 public enum HoerbuchArt implements SelectAble {
-	;
+	Hoerspiel		(1,"Hörspiel"),
+	Lesung			(2,"Lesung"),
+	szenischeLesung	(3,"szenische Lesung"),
+	SachHoerbuch	(4,"Sach-Hörbuch"),
+	LehrHoerbuch	(5,"Lehr-Hörbuch"),
+	Audioguide		(6,"Audioguide"),
+	Podcast			(7,"Podcast");
+	
 	private	int		id;
 	private	String	bezeichnung;
 	
