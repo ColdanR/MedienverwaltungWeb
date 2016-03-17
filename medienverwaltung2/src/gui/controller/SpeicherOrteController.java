7cde68a2ccdaba90e1f009f21ebf05abf6452538
Jasmin Burger <Jasmin Burger@Jasi-Think>
2016-03-17 10:26:27 +0100

    genre

--- medienverwaltung2/src/gui/controller/SpeicherOrteController.java
+++ medienverwaltung2/src/gui/controller/SpeicherOrteController.java
@@ -73,125 +73,125 @@ public class SpeicherOrteController extends Controller {
 	}
 
 	private void neuanlage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (request.getParameter("idFormat") == null) {
-			FehlerDTO dto = new FehlerDTO();
-			dto.addError("Keine Format ID beim Aufruf zum L�schen des Formates gefunden.");
-			forward(request, response, dto, "404.jsp");
-		} else if (request.getParameter("idMedium") == null) {
-			FehlerDTO dto = new FehlerDTO();
-			dto.addError("Kein Medium Type beim Aufruf zum L�schen des Formates gefunden.");
-			forward(request, response, dto, "404.jsp");
-		} else if (request.getParameter("idMediumType") == null) {
-			FehlerDTO dto = new FehlerDTO();
-			dto.addError("Kein Medium Type beim Aufruf zum L�schen des Formates gefunden.");
-			forward(request, response, dto, "404.jsp");
-		} else if (request.getParameter("idFormatType") == null) {
-			FehlerDTO dto = new FehlerDTO();
-			dto.addError("Kein Format Type beim Aufruf zum L�schen des Formates gefunden.");
-			forward(request, response, dto, "404.jsp");
-		} else {
-			try {
-				int						idSpeicherFormat	=	Integer.parseInt(request.getParameter("idFormat"));
-				int						idMedium			=	Integer.parseInt(request.getParameter("idMedium"));
-				int						idMediumType		=	Integer.parseInt(request.getParameter("idMediumType"));
-				int						idFormatType		=	Integer.parseInt(request.getParameter("idFormatType"));
-				Mediengruppe			typeMedium			=	Mediengruppe.getElementFromId(idMediumType);
-				Format					typeFormat			=	Format.getElementFromId(idFormatType);
-				if (typeMedium == null) {
-					FehlerDTO dto = new FehlerDTO();
-					dto.addError("Medium Type konnte nicht bestimmt werden.");
-					forward(request, response, dto, "404.jsp");
-				} else if (typeFormat == null) {
-					FehlerDTO dto = new FehlerDTO();
-					dto.addError("Format Type konnte nicht bestimmt werden.");
-					forward(request, response, dto, "404.jsp");
-				} else {
-					if (request.getParameter("send") != null) {
-						String	selectedArtString	=	request.getParameter("speicherortArt");
-					} else {
-						SpeicherorteDTO	dto	=	new SpeicherorteDTO("Speicherort anlegen", 0, null, Arrays.asList(typeFormat.getAllowed()), idSpeicherFormat, idMedium, idMediumType, idFormatType);
-						forward(request, response, dto, "speicherortEingabe.jsp");
-					}
-				}
-				if (request.getParameter("send") != null) {
-					String	selectedArtString	=	request.getParameter("speicherortArt");
-					// Parameter übernehmen
-					String	formatTypeString	=	request.getParameter("format");
-					if (formatTypeString == null) {
-						FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
-						dto.addError("Fehlender Parameter: FormatArt");
-						forward(request, response, dto, "speicherformatEingabe.jsp");
-					} else {
-						Format	formatType			=	Format.getElementFromId(Integer.parseInt(request.getParameter("format")));
-						if (formatType != null) {
-							List<String> errors = new ArrayList<>();
-							Formate format = logik.create(formatType);
-							if (format.getType() == Format.Digital) {
-								Digital digital = (Digital) format;
-								digital.setDateiformat(request.getParameter("dateiformat"));
-								digital.setQualitaet(request.getParameter("quali"));
-								DigitalValidator validator = new DigitalValidator();
-								if (validator.validate(digital) && logik.write()) {
-									String uri = "medium/" + type.getURIPart() + "/" + Action.Details.getURIPart() + ".html?id=" + mediumId;
-									redirect(request, response, uri);
-								} else {
-									validator.getErrors().stream().forEach(error -> {
-										errors.add(error);
-									});
-									logik.getErrors().stream().forEach(error -> {
-										errors.add(error);
-									});
-									DigitalFormateDTO	dto	=	new	DigitalFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
-									errors.stream().forEach(error -> {
-										dto.addError(error);
-									});
-									dto.setSelectedFormat(formatType);
-									dto.setDateiformat(digital.getDateiformat());
-									dto.setQualitaet(digital.getQualitaet());
-									forward(request, response, dto, "speicherformatEingabe.jsp");
-								}
-							} else if (format.getType() == Format.Analog) {
-								Analog analog = (Analog) format;
-								AnalogValidator validator = new AnalogValidator();
-								if (validator.validate(analog) && logik.write()) {
-									String uri = "medium/" + type.getURIPart() + "/" + Action.Details.getURIPart() + ".html?id=" + mediumId;
-									redirect(request, response, uri);
-								} else {
-									validator.getErrors().stream().forEach(error -> {
-										errors.add(error);
-									});
-									logik.getErrors().stream().forEach(error -> {
-										errors.add(error);
-									});
-									AnalogFormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
-									errors.stream().forEach(error -> {
-										dto.addError(error);
-									});
-									dto.setSelectedFormat(formatType);
-									forward(request, response, dto, "speicherformatEingabe.jsp");
-								}
-							} else {
-								FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
-								dto.addError("Unbekanntes Format");
-								forward(request, response, dto, "speicherformatEingabe.jsp");
-							}
-						} else {
-							FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
-							dto.addError("FormatArt konnte nicht bestimmt werden.");
-							forward(request, response, dto, "speicherformatEingabe.jsp");
-						}
-					}
-				} else {
-					FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
-					forward(request, response, dto, "speicherformatEingabe.jsp");
-				}
-			} catch (NumberFormatException e) {
-				e.printStackTrace();
-				FehlerDTO dto = new FehlerDTO();
-				dto.addError("Fehlerhafte ID beim Aufruf zum L�schen des Formates gefunden.");
-				forward(request, response, dto, "404.jsp");
-			}
-		}
+//		if (request.getParameter("idFormat") == null) {
+//			FehlerDTO dto = new FehlerDTO();
+//			dto.addError("Keine Format ID beim Aufruf zum L�schen des Formates gefunden.");
+//			forward(request, response, dto, "404.jsp");
+//		} else if (request.getParameter("idMedium") == null) {
+//			FehlerDTO dto = new FehlerDTO();
+//			dto.addError("Kein Medium Type beim Aufruf zum L�schen des Formates gefunden.");
+//			forward(request, response, dto, "404.jsp");
+//		} else if (request.getParameter("idMediumType") == null) {
+//			FehlerDTO dto = new FehlerDTO();
+//			dto.addError("Kein Medium Type beim Aufruf zum L�schen des Formates gefunden.");
+//			forward(request, response, dto, "404.jsp");
+//		} else if (request.getParameter("idFormatType") == null) {
+//			FehlerDTO dto = new FehlerDTO();
+//			dto.addError("Kein Format Type beim Aufruf zum L�schen des Formates gefunden.");
+//			forward(request, response, dto, "404.jsp");
+//		} else {
+//			try {
+//				int						idSpeicherFormat	=	Integer.parseInt(request.getParameter("idFormat"));
+//				int						idMedium			=	Integer.parseInt(request.getParameter("idMedium"));
+//				int						idMediumType		=	Integer.parseInt(request.getParameter("idMediumType"));
+//				int						idFormatType		=	Integer.parseInt(request.getParameter("idFormatType"));
+//				Mediengruppe			typeMedium			=	Mediengruppe.getElementFromId(idMediumType);
+//				Format					typeFormat			=	Format.getElementFromId(idFormatType);
+//				if (typeMedium == null) {
+//					FehlerDTO dto = new FehlerDTO();
+//					dto.addError("Medium Type konnte nicht bestimmt werden.");
+//					forward(request, response, dto, "404.jsp");
+//				} else if (typeFormat == null) {
+//					FehlerDTO dto = new FehlerDTO();
+//					dto.addError("Format Type konnte nicht bestimmt werden.");
+//					forward(request, response, dto, "404.jsp");
+//				} else {
+//					if (request.getParameter("send") != null) {
+//						String	selectedArtString	=	request.getParameter("speicherortArt");
+//					} else {
+//						SpeicherorteDTO	dto	=	new SpeicherorteDTO("Speicherort anlegen", 0, null, Arrays.asList(typeFormat.getAllowed()), idSpeicherFormat, idMedium, idMediumType, idFormatType);
+//						forward(request, response, dto, "speicherortEingabe.jsp");
+//					}
+//				}
+//				if (request.getParameter("send") != null) {
+//					String	selectedArtString	=	request.getParameter("speicherortArt");
+//					// Parameter übernehmen
+//					String	formatTypeString	=	request.getParameter("format");
+//					if (formatTypeString == null) {
+//						FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
+//						dto.addError("Fehlender Parameter: FormatArt");
+//						forward(request, response, dto, "speicherformatEingabe.jsp");
+//					} else {
+//						Format	formatType			=	Format.getElementFromId(Integer.parseInt(request.getParameter("format")));
+//						if (formatType != null) {
+//							List<String> errors = new ArrayList<>();
+//							Formate format = logik.create(formatType);
+//							if (format.getType() == Format.Digital) {
+//								Digital digital = (Digital) format;
+//								digital.setDateiformat(request.getParameter("dateiformat"));
+//								digital.setQualitaet(request.getParameter("quali"));
+//								DigitalValidator validator = new DigitalValidator();
+//								if (validator.validate(digital) && logik.write()) {
+//									String uri = "medium/" + type.getURIPart() + "/" + Action.Details.getURIPart() + ".html?id=" + mediumId;
+//									redirect(request, response, uri);
+//								} else {
+//									validator.getErrors().stream().forEach(error -> {
+//										errors.add(error);
+//									});
+//									logik.getErrors().stream().forEach(error -> {
+//										errors.add(error);
+//									});
+//									DigitalFormateDTO	dto	=	new	DigitalFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
+//									errors.stream().forEach(error -> {
+//										dto.addError(error);
+//									});
+//									dto.setSelectedFormat(formatType);
+//									dto.setDateiformat(digital.getDateiformat());
+//									dto.setQualitaet(digital.getQualitaet());
+//									forward(request, response, dto, "speicherformatEingabe.jsp");
+//								}
+//							} else if (format.getType() == Format.Analog) {
+//								Analog analog = (Analog) format;
+//								AnalogValidator validator = new AnalogValidator();
+//								if (validator.validate(analog) && logik.write()) {
+//									String uri = "medium/" + type.getURIPart() + "/" + Action.Details.getURIPart() + ".html?id=" + mediumId;
+//									redirect(request, response, uri);
+//								} else {
+//									validator.getErrors().stream().forEach(error -> {
+//										errors.add(error);
+//									});
+//									logik.getErrors().stream().forEach(error -> {
+//										errors.add(error);
+//									});
+//									AnalogFormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
+//									errors.stream().forEach(error -> {
+//										dto.addError(error);
+//									});
+//									dto.setSelectedFormat(formatType);
+//									forward(request, response, dto, "speicherformatEingabe.jsp");
+//								}
+//							} else {
+//								FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
+//								dto.addError("Unbekanntes Format");
+//								forward(request, response, dto, "speicherformatEingabe.jsp");
+//							}
+//						} else {
+//							FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
+//							dto.addError("FormatArt konnte nicht bestimmt werden.");
+//							forward(request, response, dto, "speicherformatEingabe.jsp");
+//						}
+//					}
+//				} else {
+//					FormateDTO	dto	=	new	AnalogFormateDTO("Format eingeben", request.getContextPath() + "/formate/" + Action.Neuanlage.getURIPart() + ".html", mediumId, type.getId());
+//					forward(request, response, dto, "speicherformatEingabe.jsp");
+//				}
+//			} catch (NumberFormatException e) {
+//				e.printStackTrace();
+//				FehlerDTO dto = new FehlerDTO();
+//				dto.addError("Fehlerhafte ID beim Aufruf zum L�schen des Formates gefunden.");
+//				forward(request, response, dto, "404.jsp");
+//			}
+//		}
 	}
 
 	private void bearbeiten(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
