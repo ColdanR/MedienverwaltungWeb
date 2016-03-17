003b577f51d884abf53cfabfd48a748c0f2f773c
jensw2015 <kawi@kawi-PC>
2016-03-17 08:05:45 +0100

    Dia

---
+++ medienverwaltung2/src/database/speicherorte/DBDia.java
@@ -0,0 +1,178 @@
+package database.speicherorte;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import data.speicherorte.Dia;
+
+public class DBDia extends DBSpeicherOrte<Dia> {
+
+	@Override
+	public Dia load(int id) {
+		Connection			conn	=	null;
+		PreparedStatement	stmt	=	null;
+		ResultSet			result	=	null;
+		Dia			ret		=	null;
+		try {
+			conn = getConnection();
+			stmt = conn.prepareStatement("SELECT s.bezeichnung, s.bemerkung, dia.zustand, "
+					+ "FROM speicherort s "
+					+ "INNER JOIN dia ON s.id = dia.speicherort_id "
+					+ "WHERE s.id = ?");
+			stmt.setInt(1, id);
+			result = stmt.executeQuery();
+			if (result.next() && result.isLast()) {
+				ret = new Dia();
+				ret.setDbId(id);
+				ret.setLagerOrt(result.getString(1));
+				ret.setBemerkung(result.getString(2));
+				ret.setZustand(result.getString(3));
+			} else {
+				addError("Dia mit der ID " + id + " nicht gefunden");
+			}
+			result.close();
+			result = null;
+			stmt.close();
+			stmt = null;
+			conn.close();
+			conn = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			addError("Fehler beim Laden des Dias");
+		} finally {
+			if (result != null) {
+				try {
+					result.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ret;
+	}
+
+	@Override
+	public boolean write(Dia medium, int formatId) {
+		Connection			conn	=	null;
+		PreparedStatement	stmt	=	null;
+		ResultSet			result	=	null;
+		boolean				ret		=	true;
+		try {
+			conn = getConnection();
+			conn.setAutoCommit(false);
+			if (medium.getDbId() == 0) {
+				stmt = conn.prepareStatement("INSERT INTO speicherort (bezeichnung, bemerkung, speicherformat_id) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+				stmt.setString(1, medium.getLagerOrt());
+				stmt.setString(2, medium.getBemerkung());
+				stmt.setInt(3, formatId);
+				stmt.execute();
+				result = stmt.getGeneratedKeys();
+				result.next();
+				medium.setDbId(result.getInt(1));
+				result.close();
+				result = null;
+				stmt.close();
+				stmt = conn.prepareStatement("INSERT INTO dia (speicherort_id, zustand) VALUES (?, ?)");
+				stmt.setInt(1, medium.getDbId());
+				stmt.setString(2, medium.getZustand());
+				stmt.execute();
+				stmt.close();
+				stmt = null;
+			} else {
+				stmt = conn.prepareStatement("UPDATE speicherort SET bezeichnung = ?, bemerkung = ? WHERE speicherort.id = ?");
+				stmt.setString(1, medium.getLagerOrt());
+				stmt.setString(2, medium.getBemerkung());
+				stmt.setInt(3, medium.getDbId());
+				stmt.execute();
+				stmt.close();
+				stmt = conn.prepareStatement("UPDATE dia SET zustand = ? WHERE speicherort_id = ?");
+				stmt.setString(1, medium.getZustand());
+				stmt.setInt(2, medium.getDbId());
+				stmt.execute();
+				stmt.close();
+				stmt = null;
+			}
+			conn.commit();
+			conn.setAutoCommit(true);
+			conn.close();
+			conn = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			addError("Fehler beim Schreiben des Dias");
+			ret = false;
+		} finally {
+			if (result != null) {
+				try {
+					result.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ret;
+	}
+
+	@Override
+	public List<Dia> listForFormatId(int formatId) {
+		Connection			conn	=	null;
+		PreparedStatement	stmt	=	null;
+		ResultSet			result	=	null;
+		boolean				noError	=	true;
+		List<Dia>			ret		=	new ArrayList<>();
+		try {
+			conn = getConnection();
+			stmt = conn.prepareStatement("SELECT speicherort.id FROM speicherort s"
+					+ "INNER JOIN dia ON s.id = dia.speicherort_id"
+					+ "WHERE s.speicherformat_id = ?");
+			stmt.setInt(1, formatId);
+			result = stmt.executeQuery();
+			while (result.next() && noError) {
+				Dia element = load(result.getInt(1));
+				if (element == null) {
+					noError = false;
+				} else {
+					ret.add(element);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			addError("Fehler beim Zuholen der ID Liste für SpeicherformatID " + formatId);
+			ret = null;
+		}
+		return ret;
+	}
+
+}
